03f22f61df9e1318ebb0a8383b2a1c4f1e31e919
Norman Fomferra <norman.fomferra@brockmann-consult.de>
2011-12-09 13:46:25 +0100

    added JNA test

--- beam-waterradiance-jnatest/src/main/java/jnatest/CLib.java
+++ beam-waterradiance-jnatest/src/main/java/jnatest/CLib.java
@@ -5,8 +5,8 @@ import com.sun.jna.Native;
 import com.sun.jna.Platform;
 
 /**
-* @author Norman Fomferra
-*/
+ * @author Norman Fomferra
+ */
 public interface CLib extends Library {
     CLib INSTANCE = (CLib) Native.loadLibrary((Platform.isWindows() ? "msvcrt" : "c"), CLib.class);
 
