51291f2f91d8df0aa7cdff52edf64905d43fdcbb
Norman Fomferra <norman.fomferra@brockmann-consult.de>
2011-12-15 13:07:40 +0100

    Integrated Roland's levmar_nn() C function

---
+++ beam-waterradiance-processor/src/main/java/org/esa/beam/waterradiance/LevMarNnLib.java
@@ -0,0 +1,50 @@
+package org.esa.beam.waterradiance;
+
+import com.sun.jna.Library;
+import com.sun.jna.Native;
+
+/**
+ * @author Norman Fomferra
+ */
+public interface LevMarNnLib extends Library {
+    LevMarNnLib INSTANCE = (LevMarNnLib) Native.loadLibrary("levmar_nn", LevMarNnLib.class);
+
+    /*
+     * int levmar_nn(int detector, double *input, int input_length, double *output, int output_length);
+     *
+     input[0]=solar_zenith
+     input[1]=solar_azimuth
+     input[2]=view_zenith
+     input[3]=view_azimuth
+     input[4]=surf_pressure
+     input[5]=ozone
+     input[6]=wind_x
+     input[7]=wind_y
+     input[8]=temperature
+     input[9]=salinity
+     input[10-24]=toa_radiance[0-14]
+     input[25-39]=solar_flux[0-14]
+
+     output[0-11] = RL_tosa_meris[0-11]
+     output[12-23]= RL_path_meris[0-11]
+     output[24-35]= RL_w[0-11]
+     output[36-47]= t_down_meris[0-11]
+     output[48-59]= t_up_meris[0-11]
+     output[60]=aot_550
+     output[61]=ang_865_443
+     output[62]=a_pig
+     output[63]=a_gelb
+     output[64]=a_part
+     output[65]=b_part
+     output[66]=b_wit
+     output[67]=sum_sq
+     output[68]=a_pig_stdev
+     output[69]=a_gelb_stdev
+     output[70]=a_part_stdev
+     output[71]=b_part_stdev
+     output[72]=b_wit_stedev
+     output[73]=nn_at_data[97]; //kdmin;
+     output[74]=nn_at_data[98]; //kd490;
+     */
+    int levmar_nn(int detector, double[] input, int input_length, double[] output, int output_length);
+}
---
+++ beam-waterradiance-processor/src/test/java/org/esa/beam/waterradiance/LevMarNnLibTest.java
@@ -0,0 +1,94 @@
+package org.esa.beam.waterradiance;
+
+import org.junit.Test;
+
+/**
+ * @author Norman Fomferra
+ */
+public class LevMarNnLibTest {
+    final double[] MERIS_SUN_SPECTRAL_FLUXES = {
+            1773.03,
+            1942.73,
+            1993.86,
+            1994.48,
+            1864.34,
+            1706.86,
+            1583.67,
+            1522.19,
+            1455.78,
+            1309.06,
+            1297.21,
+            1217.26,
+            990.95,
+            961.43,
+            925.89
+    };
+
+    @Test
+    public void testLib() throws Exception {
+
+        // final double latitude = 55.106583;
+        // final double longitude = 18.03734;
+        final double solar_zenith = 38.532475;
+        final double solar_azimuth = 142.5679;
+        final double view_zenith = 23.14311;
+        final double view_azimuth = 103.322136;
+        final double wind_x = 7.8687496;
+        final double wind_y = -2.525;
+        final double surf_pressure = 1017.61487;
+        final double ozone = 317.83008;
+        final double temperature = 20.0;
+        final double salinity = 12.0;
+
+        double[] toa_radiances = {
+                70.43595,
+                60.354992,
+                44.56492,
+                39.043613,
+                27.241674,
+                15.729385,
+                11.943042,
+                10.802422,
+                8.662219,
+                6.4378233,
+                2.8837085,
+                5.4307566,
+                3.2948744,
+                3.0640657,
+                2.1505015
+        };
+
+        final double[] input = new double[40];
+        input[0] = solar_zenith;
+        input[1] = solar_azimuth;
+        input[2] = view_zenith;
+        input[3] = view_azimuth;
+        input[4] = surf_pressure;
+        input[5] = ozone;
+        input[6] = wind_x;
+        input[7] = wind_y;
+        input[8] = temperature;
+        input[9] = salinity;
+        for (int i = 0; i < 15; i++) {
+            input[i + 10] = toa_radiances[i];
+            input[i + 25] = MERIS_SUN_SPECTRAL_FLUXES[i];
+        }
+        final double[] output = new double[75];
+
+        final LevMarNnLib lib = LevMarNnLib.INSTANCE;
+
+
+        int result;
+        final int N = 100;
+        final long t0 = System.currentTimeMillis();
+        for (int i = 0; i < N; i++) {
+            input[10] += 0.001 * Math.random();
+            result = lib.levmar_nn(181, input, input.length, output, output.length);
+        }
+        final long t1 = System.currentTimeMillis();
+        System.out.println("time " + (t1 - t0) / 1000.0 + " s");
+
+
+        //assertEquals(0, i);
+    }
+}
