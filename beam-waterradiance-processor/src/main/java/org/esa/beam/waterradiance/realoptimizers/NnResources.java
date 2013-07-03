86a323a5e49986d3166d94d655529192f2386e39
Tonio Fincke <tonio.fincke@brockmann-consult.de>
2013-07-03 14:24:58 +0200

    Installed ressources are used

--- beam-waterradiance-processor/src/main/java/org/esa/beam/waterradiance/realoptimizers/NnResources.java
+++ beam-waterradiance-processor/src/main/java/org/esa/beam/waterradiance/realoptimizers/NnResources.java
@@ -1,20 +1,27 @@
 package org.esa.beam.waterradiance.realoptimizers;
 
 
+import com.bc.ceres.core.ProgressMonitor;
+import com.bc.ceres.core.runtime.internal.Platform;
+import org.esa.beam.ocnnrd.OcNnRdOperator;
+import org.esa.beam.util.ResourceInstaller;
+import org.esa.beam.util.SystemUtils;
+
+import java.io.File;
+import java.io.IOException;
 import java.net.URL;
 
 public class NnResources {
 
-    private static final String AUXDATA_PATH = "../../../../../auxdata/";
-    private static final String SMILE_CORR_PATH = "../../../../../auxdata/smile/";
-    private static final String AC_FWD_PATH = "../../../../../auxdata/oc_cci_20121127/ac_forward_all/";
+    private static final String AUXDATA_PATH = "auxdata/";
+    private static final String SMILE_CORR_PATH = "auxdata/smile/";
+    private static final String AC_FWD_PATH = "auxdata/oc_cci_20121127/ac_forward_all/";
 
     public String getNormNetPath() {
         return getResourcePath(AUXDATA_PATH + "27x41x27_23.3.net");
     }
 
     public String getNetWaterPath() {
-//        return getResourcePath(AUXDATA_PATH + "for_water_rw29_20120318/37x17_754.1.net");
         return getResourcePath(AUXDATA_PATH + "for_water_rw29_20120318/37x77x97_86.7.net");
     }
 
@@ -43,7 +50,17 @@ public class NnResources {
     }
 
     private String getResourcePath(String name) {
-        final URL resource = NnResources.class.getResource(name);
-        return resource.getPath();
+        String homepath;
+        if(System.getenv().get("OS").contains("Windows")) {
+            homepath = System.getenv().get("HOMEPATH");
+        } else {
+            homepath = System.getenv().get("HOME");
+        }
+        String auxpath = "/.beam/beam-waterradiance-processor/";
+        StringBuilder builder = new StringBuilder();
+        builder.append(homepath);
+        builder.append(auxpath);
+        builder.append(name);
+        return builder.toString();
     }
 }
